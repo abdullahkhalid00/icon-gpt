e111b48149cacf66d1e448d57616efbc4f77b7db
Abdullah Khalid <iabdulahk@gmail.com>
2024-09-26 07:54:24 +0500

    Add FastAPI endpoint

---
+++ app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from search import get_search_results
+
+app = FastAPI()
+
+class SearchResult(BaseModel):
+    emoji: str
+    tags: List[str]
+    short_description: str
+    long_description: str
+    score: float
+
+class SearchResponse(BaseModel):
+    results: List[SearchResult]
+
+
+@app.get('/')
+async def read_root():
+    return 'FastAPI endpoint running at /search', 200
+
+@app.post('/search', response_model=SearchResponse, description='Runs a vector search on the deployed MongoDB collection.')
+async def search_database(query: str):
+    try:
+        results = get_search_results(query)
+        return {'results': results}
+    except Exception as e:
+        return {'error': str(e)}
