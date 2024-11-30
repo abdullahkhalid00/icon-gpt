86321de6cfea991ca2cb0e0cfe10b0caf20d49a4
Abdullah Khalid <iabdulahk@gmail.com>
2024-11-30 19:35:14 +0500

    Fix CORS

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # allow frontend comms
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://127.0.0.1:3000'],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
