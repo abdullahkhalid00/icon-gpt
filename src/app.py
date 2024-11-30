61f1b59f579e35947231dc09455aee4f62b9809e
Abdullah Khalid <iabdulahk@gmail.com>
2024-11-30 13:48:45 +0500

    Allow CORS for frontend comms

--- src/app.py
+++ src/app.py
@@ -6,6 +6,15 @@ from src.search import get_search_results
 
 app = FastAPI()
 
+# allow frontend comms
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://127.0.0.1:3000'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 class SearchResult(BaseModel):
     emoji: str
     # TODO: add unicode: str
